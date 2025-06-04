9811eaf5fc53dbd98c57977cdbd32bd1c175ccde
ArnabDas2003 <121857667+ArnabDas2003@users.noreply.github.com>
2025-06-04 15:58:48 +0530

    Time: 1 ms (46.22%), Space: 77.1 MB (72.96%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,11 +11,11 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* head = NULL;    // Head of the result list
-    ListNode* tail = NULL;    // Tail to keep adding nodes
+         ListNode* dummy = new ListNode();  // dummy node to simplify logic
+    ListNode* current = dummy;
     int carry = 0;
 
-    while (l1 != NULL || l2 != NULL || carry > 0) {
+    while (l1 != NULL || l2 != NULL || carry != 0) {
         int sum = carry;
 
         if (l1 != NULL) {
@@ -28,30 +28,14 @@ public:
             l2 = l2->next;
         }
 
-        int digit = sum % 10;
         carry = sum / 10;
-
-        ListNode* newNode = new ListNode(digit);
-
-        if (head == NULL) {
-            head = tail = newNode;
-        } else {
-            tail->next = newNode;
-            tail = tail->next;
-        }
+        current->next = new ListNode(sum % 10);
+        current = current->next;
     }
 
-    return head;
+    return dummy->next;
+
 
         
     }
-
-    void printList(ListNode* node) {
-    while (node != NULL) {
-        cout << node->val;
-        if (node->next != NULL) cout << " -> ";
-        node = node->next;
-    }
-    cout << endl;
-}
 };
