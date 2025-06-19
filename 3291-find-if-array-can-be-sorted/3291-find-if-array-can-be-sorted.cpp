f002701bafdbdd5ed724388cbe477b0bdc03a50d
ArnabDas2003 <121857667+ArnabDas2003@users.noreply.github.com>
2025-06-19 16:41:13 +0530

    Time: 16 ms (10.86%), Space: 31.6 MB (81.9%) - LeetHub

---
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            count += (n & 1);
+            n >>= 1;
+        }
+        return count;
+    }
+
+    bool canSortArray(vector<int>& nums) {
+        int n = nums.size();
+
+        // Bubble sort with swap allowed only if set bits count matches
+        for (int i = 0; i < n; i++) {
+            bool check = false;
+            for (int j = 0; j < n - 1; j++) {
+                if (countSetBits(nums[j]) == countSetBits(nums[j + 1]) && nums[j] > nums[j + 1]) {
+                    swap(nums[j], nums[j + 1]);
+                    check = true;
+                }
+            }
+            if (!check) break;
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) return false;
+        }
+
+        return true;
+    }
+};
