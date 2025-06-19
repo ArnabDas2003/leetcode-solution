82a8b1400d81a990fd81865b289fc8196d0e19f9
ArnabDas2003 <121857667+ArnabDas2003@users.noreply.github.com>
2025-06-19 16:46:28 +0530

    Time: 15 ms (11.31%), Space: 31.7 MB (57.92%) - LeetHub

--- 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
+++ 3291-find-if-array-can-be-sorted/3291-find-if-array-can-be-sorted.cpp
@@ -4,7 +4,8 @@ public:
         int count = 0;
         while (n > 0) {
             count += (n & 1);
-            n >>= 1;
+            // n >>= 1;
+            n/=2;
         }
         return count;
     }
