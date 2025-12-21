ca2380aeed1a54325f609a4f706dc6a58abf5f85
ArnabDas2003 <121857667+ArnabDas2003@users.noreply.github.com>
2025-12-21 19:23:58 +0530

    Time: 0 ms (100%), Space: 8.9 MB (65.73%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,31 +1,27 @@
-#include <iostream>
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> st;
-
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s[i];
-
-            if (ch == '(' || ch == '{' || ch == '[') {
+        stack<char>st;
+        for(int i=0;i<s.size();i++){
+            char ch=s[i];
+            if((ch=='('||ch=='{'||ch=='[')){
                 st.push(ch);
-            } else {
-                if (st.empty()) return false;
-
-                char top = st.top();
-                if ((ch == ')' && top == '(') ||
-                    (ch == '}' && top == '{') ||
-                    (ch == ']' && top == '[')) {
+            }
+            else{
+                if(st.empty()){
+                    return false;
+                }
+                if((ch==')'&&st.top()=='(')||(ch=='}'&&st.top()=='{')||(ch==']'&&st.top()=='[')){
                     st.pop();
-                } else {
+                }else{
                     return false;
                 }
             }
         }
-
-        return st.empty();
+        if(st.empty()){
+            return true;
+        }else{
+            return false;
+        }
     }
 };
